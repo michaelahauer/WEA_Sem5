162bc56efa9863c9a50deea2883ef735e74a0177
Michaela Hauer <mimohauer@gmail.com>
2022-01-02 18:44:04 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aaas-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'AaaSWeb';
+}
